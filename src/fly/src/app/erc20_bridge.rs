ffc8a5c9353107106050336eb5a9e9bce51f97e2
veeso <christian.visintin@veeso.dev>
2024-01-25 17:30:50 +0100

    feat: swap pool

--- src/fly/src/app/erc20_bridge.rs
+++ src/fly/src/app/erc20_bridge.rs
@@ -1,13 +1,24 @@
 mod swap_fee;
+mod swap_pool;
 
-use did::fly::FlyResult;
+use did::fly::{FlyError, FlyResult, PicoFly};
+use did::H160;
+use icrc::icrc1::account::Account;
 
 use self::swap_fee::SwapFee;
+use self::swap_pool::SwapPool;
+use crate::app::{Balance, Configuration};
+use crate::utils;
 
 /// ERC20 Bridge
 pub struct Erc20Bridge;
 
 impl Erc20Bridge {
+    /// Swaps the ERC20 token to the FLY token.
+    pub async fn swap(recipient: H160, amount: PicoFly) -> FlyResult<()> {
+        todo!();
+    }
+
     /// Returns the swap fee.
     pub fn get_swap_fee() -> u64 {
         SwapFee::get_swap_fee()
