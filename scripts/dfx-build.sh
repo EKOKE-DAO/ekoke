5af63fdfe7888f507b670586d73bc827336d336e
veeso <christian.visintin@veeso.dev>
2024-02-26 11:51:52 +0100

    fix: dfx-build script

---
+++ scripts/dfx-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# build frontend
+cd ./src/ekoke_erc20_swap_frontend/
+yarn
+mkdir -p node_modules/web3/dist/
+wget -O node_modules/web3/dist/web3.min.js "https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js"
+yarn build:release
+cd -
+
+# build rust canisters
+dfx build
