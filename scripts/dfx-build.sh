46674ac078f87646494bd992a1df76e7d531700b
veeso <christian.visintin@veeso.dev>
2024-04-02 15:48:02 +0200

    fix: build

--- scripts/dfx-build.sh
+++ scripts/dfx-build.sh
@@ -5,7 +5,7 @@ cd ./src/ekoke_erc20_swap_frontend/
 yarn
 mkdir -p node_modules/web3/dist/
 wget -O node_modules/web3/dist/web3.min.js "https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js"
-yarn build:release
+yarn build
 cd -
 
 # build rust canisters
