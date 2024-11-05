1d0b2a54c7f4691e1945598d5a52d4fceb815425
veeso <christian.visintin@veeso.dev>
2024-11-05 11:09:58 +0100

    fix: dfx 0.24.1

--- scripts/dfx-build.sh
+++ scripts/dfx-build.sh
@@ -5,6 +5,7 @@ cd ./src/ekoke_erc20_swap_frontend/
 yarn
 mkdir -p node_modules/web3/dist/
 wget -O node_modules/web3/dist/web3.min.js "https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js"
+npx -y update-browserslist-db@latest
 yarn build
 cd -
 
