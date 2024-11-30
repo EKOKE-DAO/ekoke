8b5a12c62c012fd0769e35d7e62b12ad4c75a964
veeso <christian.visintin@veeso.dev>
2024-11-30 17:16:13 +0100

    feat: burn method

--- ethereum/app/src/js/web3/contracts/Ekoke.ts
+++ ethereum/app/src/js/web3/contracts/Ekoke.ts
@@ -365,6 +365,19 @@ export const ABI = [
     type: 'function',
   },
   {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'burn',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
     inputs: [],
     name: 'decimals',
     outputs: [
