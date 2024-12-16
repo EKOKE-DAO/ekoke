cebbf6b571279079faf4177447b7ff2ab564dcce
veeso <christian.visintin@veeso.dev>
2024-12-16 18:38:28 +0100

    fix: step price at 20k

--- ethereum/app/src/js/web3/contracts/EkokePresale.ts
+++ ethereum/app/src/js/web3/contracts/EkokePresale.ts
@@ -370,7 +370,7 @@ interface ContractAddress {
 export const CONTRACT_ADDRESS: ContractAddress = {
   [ChainId.Goerli]: '',
   [ChainId.Kovan]: '',
-  [ChainId.Mainnet]: '0x0AA24850527dAC93EFA962E9D8a8E08f8DC083DF',
+  [ChainId.Mainnet]: '0xa5fF566D68E3521929F47447b975C4683618C35f',
   [ChainId.Rinkeby]: '',
   [ChainId.Ropsten]: '',
   [ChainId.Sepolia]: '0x10B6bDB762aE3bAB9C6A097455252171b7F80457',
